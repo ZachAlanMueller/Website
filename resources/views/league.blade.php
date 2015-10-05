400df9879a1205216ba40ebd809537b9c4140862
ZachAlanMueller <zach.a.mueller@gmail.com>
2015-10-04 19:54:25 -0500

    Undoing blade test

--- resources/views/league.blade.php
+++ resources/views/league.blade.php
@@ -10,7 +10,36 @@
     <body>
         <div class="container">
             <div class="content">
-                @include('includes.bodytest')
+                <style>
+                html, body {
+                    height: 100%;
+                }
+
+                body {
+                    margin: 0;
+                    padding: 0;
+                    width: 100%;
+                    display: table;
+                    font-weight: 100;
+                    font-family: 'Lato';
+                }
+
+                .container {
+                    text-align: center;
+                    display: table-cell;
+                    vertical-align: middle;
+                }
+
+                .content {
+                    text-align: center;
+                    display: inline-block;
+                }
+
+                .title {
+                    font-size: 96px;
+                }
+                </style>
+                <div class="title">League of Legends<br>More League...</div>
             </div>
         </div>
     </body>
