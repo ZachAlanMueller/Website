35ccbb2e121c31986448a428c6428026bc1b94bb
ZachAlanMueller <Zach.a.mueller@gmail.com>
2015-10-21 06:46:51 -0500

    Attempt to add Illuminate

--- config/app.php
+++ config/app.php
@@ -140,6 +140,7 @@ return [
         /*
          * Application Service Providers...
          */
+        'Illuminate\Html\HtmlServiceProvider',
         App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
@@ -193,6 +194,8 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        'HTML'      => 'Illuminate\Html\HtmlFacade',
+        'Form'      => 'Illuminate\Html\FormFacade',
 
     ],
 
