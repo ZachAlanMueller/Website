e2386ca9eb8b9ae58da63c2a0ce00fbcdd84678e
ZachAlanMueller <Zach.a.mueller@gmail.com>
2015-10-31 14:28:35 -0500

    Fixing Tunnel

--- app/Http/Controllers/work_controller.php
+++ app/Http/Controllers/work_controller.php
@@ -11,7 +11,7 @@ class work_controller extends Controller {
      * @return Response
      */
     public function check_routes() {
-        $results = DB::connection('chowtime')->select('select count(*) as count from locations');
+        $results = \DB::connection('chowtime')->select('select count(*) as count from locations');
         return View::make('work.blade', $results);
     }
 
