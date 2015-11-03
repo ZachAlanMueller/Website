859ad0b6cc429c2ca253200e6a882aa84b3e72a7
ZachAlanMueller <Zach.a.mueller@gmail.com>
2015-11-03 11:58:14 -0600

    Rename and define database in controller rather than in config

--- config/database.php
+++ config/database.php
@@ -56,6 +56,7 @@ return [
             'driver'    => 'mysql',
             'host'      => '127.0.0.1',
             'port'      => '3307',
+            'database'  => 'pls_chowtime',
             'username'  => 'zach',
             'password'  => 'smartsnack15',
             'charset'   => 'utf8',
