2a926741c5589e915fe8813a33219232980347b0
ZachAlanMueller <Zach.a.mueller@gmail.com>
2015-11-03 11:57:27 -0600

    Rename and define database in controller rather than in config

--- config/database.php
+++ config/database.php
@@ -52,7 +52,7 @@ return [
             'prefix'   => '',
         ],
 
-        'chowtime' => [
+        'parlevel' => [
             'driver'    => 'mysql',
             'host'      => '127.0.0.1',
             'port'      => '3307',
